a2d955c771ac79239641cabdfc03efe14e4616ae
MaevaWolff <maeva.wolff@hetic.net>
2024-03-06 23:22:56 +0100

    refactor: use white to default text color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-h-screen w-full bg-mobile px-4 py-4 sm:bg-tablet lg:bg-desktop"
+    class="min-h-screen w-full bg-mobile px-4 py-4 text-white sm:bg-tablet lg:bg-desktop"
   >
     <header>header</header>
 
