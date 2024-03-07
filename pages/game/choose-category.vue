c21a01ddfa3596abf6c9a051f99caa0c9ed6799a
MaevaWolff <maeva.wolff@hetic.net>
2024-03-07 21:55:18 +0100

    feat: create choose-category page

--- pages/game/choose-category.vue
+++ pages/game/choose-category.vue
@@ -1,3 +1,34 @@
+<script setup>
+const categoriesName = getCategoriesName();
+</script>
+
 <template>
-  <h1>Choose category</h1>
+  <header
+    class="flex w-full flex-row items-center justify-between lg:grid lg:grid-cols-[auto_auto_auto]"
+  >
+    <ButtonRound size="small" @click="$router.back()">
+      <img src="~assets/svg/icons/icon-back.svg" />
+    </ButtonRound>
+
+    <h1
+      class="bg-gradient-to-b from-[#67B6FF] to-white bg-clip-text text-5xl text-transparent md:text-8xl lg:text-9xl"
+    >
+      Pick a category
+    </h1>
+  </header>
+
+  <main class="min-h-[70vh] w-full">
+    <section
+      class="flex w-full flex-wrap justify-center gap-4 md:gap-8 lg:gap-12"
+    >
+      <NuxtLink
+        v-for="categoryName in categoriesName"
+        :key="categoryName"
+        class="button-game-default flex w-full items-center justify-center rounded-2xl bg-game-blue px-4 py-5 text-xl uppercase hover:bg-[#5B8AFF] md:h-[140px] md:w-[300px] md:rounded-3xl md:text-5xl"
+        :to="'/game?category=' + categoryName"
+      >
+        {{ categoryName }}
+      </NuxtLink>
+    </section>
+  </main>
 </template>
---
+++ utils/getCategoriesName.ts
@@ -0,0 +1,5 @@
+import { data } from "./data";
+
+export default function getCategoriesName() {
+  return Object.keys(data.categories);
+}
