4bc0ebeaf96ab25ef800cad2c85a016ca3c6e3c8
MaevaWolff <maeva.wolff@hetic.net>
2024-03-13 04:23:48 +0100

    feat: create Button component

---
+++ components/shared/Button.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+type ButtonProps = {
+  variant: "primary" | "secondary";
+};
+
+const props = defineProps<ButtonProps>();
+</script>
+
+<template>
+  <button
+    class="rounded-full px-16 py-4 text-2xl uppercase transition-all duration-300"
+    :class="{
+      'button-game-default bg-game-blue hover:bg-[#5B8AFF] ':
+        props.variant === 'primary',
+      'button-game-secondary bg-[#DD7AFF] hover:bg-[#cc48f8]':
+        props.variant === 'secondary',
+    }"
+  >
+    <slot />
+  </button>
+</template>
