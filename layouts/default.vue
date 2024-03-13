81e640c9dc8aec663b0e17264a6614b5802c9fd2
MaevaWolff <maeva.wolff@hetic.net>
2024-03-13 04:23:34 +0100

    feat: add letter spacing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex min-h-screen w-full flex-col items-center justify-center gap-24 bg-mobile bg-cover bg-no-repeat px-4 py-4 text-white sm:bg-tablet lg:bg-desktop lg:px-28"
+    class="flex min-h-screen w-full flex-col items-center justify-center gap-24 bg-mobile bg-cover bg-no-repeat px-4 py-4 tracking-wider text-white sm:bg-tablet lg:bg-desktop lg:px-28"
   >
     <slot />
   </div>
