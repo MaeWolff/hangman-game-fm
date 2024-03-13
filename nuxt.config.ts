27015f42d37bb2cd93e491a490d9a8bf8ab9da08
MaevaWolff <maeva.wolff@hetic.net>
2024-03-13 03:24:46 +0100

    chore: install nuxt-headlessui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxt/test-utils/module"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@nuxt/test-utils/module",
+    "nuxt-headlessui",
+  ],
+  headlessui: {
+    prefix: "Headless",
+  },
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "tailwind.config",
