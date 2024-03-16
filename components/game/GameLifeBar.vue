19f70bdd80c7b02cca9b955abe99c30ef0185749
MaevaWolff <maeva.wolff@hetic.net>
2024-03-16 05:28:21 +0100

    refactor: add transition to progress bar

--- components/game/GameLifeBar.vue
+++ components/game/GameLifeBar.vue
@@ -13,7 +13,7 @@ function getLifePercent() {
 
 <template>
   <progress
-    class="h-4 w-[80px] rounded-full border-[3px] border-white md:w-[140px] [&::-webkit-progress-bar]:rounded-full [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:rounded-full [&::-webkit-progress-value]:bg-game-blue"
+    class="h-4 w-[80px] rounded-full border-[3px] border-white md:w-[140px] [&::-webkit-progress-bar]:rounded-full [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:rounded-full [&::-webkit-progress-value]:bg-game-blue [&::-webkit-progress-value]:transition-all"
     :max="props.maxLife"
     :value="props.life"
     :style="{ width: getLifePercent + '%' }"
