5bfdc77b59fa1e2f59b63be58800dec0fe81f034
MaevaWolff <maeva.wolff@hetic.net>
2024-03-18 00:49:19 +0100

    chore: initialize cypress configuration

---
+++ cypress/cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000/",
+    specPattern: "./cypress/src/integration/**/*.feature",
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions,
+    ): Promise<Cypress.PluginConfigOptions> {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({ plugins: [createEsbuildPlugin(config)] }),
+      );
+      return config;
+    },
+  },
+});
