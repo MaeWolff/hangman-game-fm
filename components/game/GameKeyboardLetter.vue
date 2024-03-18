1393fd4861c8b6c0a6744c77ebe7fa6d298dc6b9
MaevaWolff <maeva.wolff@hetic.net>
2024-03-18 05:14:43 +0100

    feat: add sound on keyboard letter

--- components/game/GameKeyboardLetter.vue
+++ components/game/GameKeyboardLetter.vue
@@ -4,11 +4,18 @@ type GameKeyboardLetterProps = {
 };
 
 const props = defineProps<GameKeyboardLetterProps>();
+const emit = defineEmits(["clickKeyboardLetter"]);
+const { play } = useAudio("pop-bubble");
+
+function onClick() {
+  play();
+  emit("clickKeyboardLetter", props.letter);
+}
 </script>
 
 <template>
   <button
-    @click="$emit('clickKeyboardLetter', props.letter)"
+    @click="onClick"
     class="rounded-lg bg-white p-6 text-2xl uppercase text-game-dark-navy hover:bg-slate-300 disabled:cursor-default disabled:opacity-25 md:px-8 md:text-4xl lg:px-12"
   >
     {{ props.letter }}
