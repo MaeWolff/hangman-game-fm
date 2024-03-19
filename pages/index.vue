ed195dab1bc0dc1195c99f91f155e817c181269c
MaevaWolff <maeva.wolff@hetic.net>
2024-03-19 02:38:02 +0100

    fix: add min-width for the hangman game logo

--- pages/index.vue
+++ pages/index.vue
@@ -7,12 +7,17 @@ useSeoMeta({
 <template>
   <BouttonSound />
 
-  <main class="flex w-full items-center justify-center">
+  <main class="mt-24 flex w-full items-center justify-center md:mt-0">
     <div
       class="shadow-game relative flex h-fit w-full max-w-[560px] flex-col items-center gap-14 rounded-[48px] bg-gradient-to-t from-[#344ABA] to-[#00147983] px-9 py-24"
     >
-      <h1 class="absolute -top-24">
-        <NuxtImg src="assets/logo.svg" alt="logo hangman-game" />
+      <h1 class="absolute -top-14 md:-top-16">
+        <NuxtImg
+          src="assets/logo.svg"
+          alt="logo hangman-game"
+          class="w-[16rem]"
+          placeholder
+        />
       </h1>
 
       <ButtonRound size="large" @click="$router.push('/game/choose-category')">
